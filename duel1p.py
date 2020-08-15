703e1d8c11a61380a9c2f8267835be3f1c2dd1ae
JeremyLARDENOIS <jeremy.lardenois@hotmail.fr>
2020-08-15 21:13:07 +0200

    Phase de préparation fini

--- duel1p.py
+++ duel1p.py
@@ -16,14 +16,14 @@ def main(self):
       """
       Classe définissant les caracteristiques d'un joueur :
       """
-      def __init__(slf, name = ""):
+      def __init__(j, name = ""):
         """Constructeur de notre classe"""
-        self.name = name
-        self.pv=50       #PV de base
-        self.atq=15      #Attaque de base
-        self.soin=5      #Soin de base
-        self.catq=10     #Contre attaque       
-        self.coutca=5    #Cout de la contre attaque
+        j.name = name
+        j.pv=50       #PV de base
+        j.atq=15      #Attaque de base
+        j.soin=5      #Soin de base
+        j.catq=10     #Contre attaque       
+        j.coutca=5    #Cout de la contre attaque
 
 
 #################################################"
@@ -73,118 +73,128 @@ def main(self):
 
 
     def next():
-        '''Permet de passer au print suivant'''
+        '''Permet de passer au add_dial suivant'''
         if self.dialog_number < len(self.dialogs)-1:
             self.dialog_number += 1
             upload()
 
     bquit = ("bouton","Quitter",exit)
-    bnext = ("bouton","Next",next)
+    bnext = ("bouton","Ok",next)
 
     add_dial("hello",bquit,bnext)
-    add_dial("yo",bquit,bnext)
-    add_dial("Fin",bquit)
+    #add_dial("yo",bquit,bnext)
+    #add_dial("Fin",bquit)
 
     upload() #Voir pour mettre upload dans add_dial
-
 
     #exit()
 
 
 
 ###############################################"
-    """
+    
     nb_tours = 0
-    print("\nBienvenue sur la plateforme de Combat po.py \n\
+    add_dial("\nBienvenue sur la plateforme de Combat po.py \n\
 Le but du jeu est de faire descendre les points de vie (pv) de l'adversaire à 0 \n\
-Vous commencez avec "+ str(Joueur().pv)+" pv. \n\
-Pour cela, vous disposez de 3 possibilités: \n")
+Vous commencez avec "+ str(Joueur().pv)+" pv. \n",bnext)
 
-    print("- Une attaque qui inflige "+str(Joueur().atq)+" pv \n\
+
+
+    add_dial("Pour cela, vous aurez 3 possibilités d'actions :\n\
+- Une attaque qui inflige "+str(Joueur().atq)+" pv \n\
 - Un soin qui restore "+str(Joueur().soin)+" pv \n\
 - Une contre-attaque, annule les dégats de l'adversaires et retourne "+ str(Joueur().catq)+"\
  contre lui sous reserve de perdre "+str(Joueur().catq)+" \
- pv si le contre-attaquant ne se fait pas attaqué \n\
-\nQUE LE COMBAT COMMENCE !\n")
+ pv si le contre-attaquant ne se fait pas attaqué \n",bnext)
 
+    add_dial("QUE LE COMBAT COMMENCE !\n",bnext)
+
+    
     #j1.name,j2.name = recv2()
 
     #Dans le cadre d'une future amélioration
-    j1 = Joueur(input("\nMais avant tout, quel est votre nom ?\n-> "))
+    #j1 = Joueur(input("\nMais avant tout, quel est votre nom ?\n-> "))
+    j1 = Joueur("Vous")
     
     j2 = Joueur("Le méchant")
  
+    '''
     while ((j1.pv > 0)and(j2.pv > 0)):
         nb_tours +=1
         
-        print("=========================================================================")
-        print("tour n°"+str(nb_tours)+" :")
-        print("\n"+j1.name+" = "+str(j1.pv)+" pv & "+j2.name+" = "+str(j2.pv)+" pv" )
+        msg = "tour n°"+str(nb_tours)+" :\n"
+        msg += "\n"+j1.name+" = "+str(j1.pv)+" pv & "+j2.name+" = "+str(j2.pv)+" pv\n" 
         
         #TOUR DES DEUX JOUEURS
-        print("C'est à votre tour\n")
-        print("Que voulez-vous faire ?")
-        print("1 : Attaque")
-        print("2 : Soin")
-        print("3 : Contre-attaque")
+        msg += "C'est à votre tour\n\n"
+        msg += "Que voulez-vous faire ?\n"
+        msg += "1 : Attaque\n"
+        msg += "2 : Soin\n"
+        msg += "3 : Contre-attaque\n"
 
+        add_dial(msg,bquit)
+
+        
         #Réception des choix
         #choix1, choix2 = recv2()
         choix1 = input("Quel est votre choix ?\n")
         choix2 = str(randint(1,3))
-        print(choix2)
+        add_dial(choix2)
 
         #MISE EN CONFRONTATION DES CHOIX
         #Les soins sont executées avant les attaques
         if (choix1 == "2"): #Soin du J1
-            print(j1.name+" se soigne !")
+            add_dial(j1.name+" se soigne !")
             gain = min(Joueur().pv,j1.pv+j1.soin) - j1.pv #On ne souhaite pas dépasser la limite de pv de base
             j1.pv += gain
-            print(j1.name+" a gagné "+str(gain)+" pv.\n")
+            add_dial(j1.name+" a gagné "+str(gain)+" pv.\n")
         if (choix2 == "2"): #Soin du J2
-            print(j2.name + " se soigne !")
+            add_dial(j2.name + " se soigne !")
             gain = min(Joueur().pv,j2.pv+j2.soin) - j2.pv #On ne souhaite pas dépasser la limite de pv de base
             j2.pv += gain
-            print(j2.name+" a gagné "+str(gain)+" pv.\n")
+            add_dial(j2.name+" a gagné "+str(gain)+" pv.\n")
         if (choix1 == "1"): #Atq du J1
-            print(j1.name +" attaque !")
+            add_dial(j1.name +" attaque !")
             if (choix2 == "3"): #Catq du J2
-                print("Mais "+j2.name+" contre-attaque !")
+                add_dial("Mais "+j2.name+" contre-attaque !")
                 j1.pv -= j2.catq
                 #j2.pv -= j2.coutca
-                print(j1.name+" a perdu "+str(j2.atq)+" pv.\n")
-                #print(j2.name+" a perdu "+str(j2.coutca)+" pv.\n")
+                add_dial(j1.name+" a perdu "+str(j2.atq)+" pv.\n")
+                #add_dial(j2.name+" a perdu "+str(j2.coutca)+" pv.\n")
             else:
                 j2.pv -= j1.atq
-                print(j2.name+" a perdu "+str(j1.atq)+" pv.\n")
+                add_dial(j2.name+" a perdu "+str(j1.atq)+" pv.\n")
         if (choix2 == "1"): #Atq du J2
-            print(j2.name+" attaque !")
+            add_dial(j2.name+" attaque !")
             if (choix1 == "3"): #Catq du J1
-                print("Mais "+j1.name+" contre-attaque !")
-                #print("Mais Joueur 1 contre-attaque et perds "+str(j1.coutca)+" pv!")
+                add_dial("Mais "+j1.name+" contre-attaque !")
+                #add_dial("Mais Joueur 1 contre-attaque et perds "+str(j1.coutca)+" pv!")
                 j2.pv -= j1.catq
                 #j1.pv -= j1.coutca
-                print(j2.name+" a perdu "+str(j1.catq)+" pv.\n")
-                #print(j1.name+" a perdu "+str(j1.coutca)+" pv.\n")
+                add_dial(j2.name+" a perdu "+str(j1.catq)+" pv.\n")
+                #add_dial(j1.name+" a perdu "+str(j1.coutca)+" pv.\n")
             else:
                 j1.pv -= j2.atq
-                print(j1.name+" a perdu "+str(j2.atq)+" pv.\n")
+                add_dial(j1.name+" a perdu "+str(j2.atq)+" pv.\n")
         if ((choix1 == "3")and(choix2 != "1")):
-                print(j1.name +" contre-attaque !")
-                print("Mais c'est inefficace.\n")
+                add_dial(j1.name +" contre-attaque !")
+                add_dial("Mais c'est inefficace.\n")
                 j1.pv -= j1.coutca
-                print(j1.name+" a perdu "+str(j1.coutca)+" pv.\n")
+                add_dial(j1.name+" a perdu "+str(j1.coutca)+" pv.\n")
         if ((choix2 == "3")and(choix1 != "1")):
-                print(j2.name+" contre-attaque !")
-                print("Mais c'est inefficace\n")
+                add_dial(j2.name+" contre-attaque !")
+                add_dial("Mais c'est inefficace\n")
                 j2.pv -= j2.coutca
-                print(j2.name+" a perdu "+str(j2.coutca)+" pv.\n")
+                add_dial(j2.name+" a perdu "+str(j2.coutca)+" pv.\n")
 
     if ((j1.pv <= 0)and(j2.pv <= 0)):
-        print("Les deux joueurs n'ont plus de pv, c'est une égalité !")
+        add_dial("Les deux joueurs n'ont plus de pv, c'est une égalité !")
     else:
         if (j2.pv <= 0):
-            print(j1.name+" a gagné !")
+            add_dial(j1.name+" a gagné !")
         if (j1.pv <= 0):
-            print(j2.name+" a gagné !")
-    """
+            add_dial(j2.name+" a gagné !")
+    '''
+
+    add_dial("END",bquit)
+            
