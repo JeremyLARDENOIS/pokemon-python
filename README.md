3b1b1f19fae87b687705bd0a9eac27eea60698d3
jerem <jeremy.lardenois@hotmail.fr>
2021-04-10 14:13:42 +0200

    Build: Translate all the programs + some fixes

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # pokemon-python
-une plateforme de combat en ligne au tour par tour
+A simple combat platform online in round by round
 
-## Pour une utilisation simple
-Vous pouvez lancer le programme serveur.py ainsi que deux machines pour une simple verification du bon fonctionnement du programme
+## For a simple use 
+You can use `server.py` for create a server and 2 `client.py` to try it
 
-## Pour une utilisation plus avancé
-Vous pouvez lancer le programme depuis un serveur distant, si celui-ci est bien configuré au niveau de la redirection des ports
-Attention a bien changé l'addresse ip dans le champ hostname des clients
+## Why this project ?
+This project was initialy a project that i made to enter in an engineer school, and it worked! 
 
-Il arrive que je laisse un conteneur docker du serveur python tourner sur ma raspberry, elle n'est pas tout le temps active mais vous pouvez toujours tester :)
+It's based on Pokemon and the game of Shi Fu Mi, where each attack do same difference of damage in function of your choose
+## To Do
+- Verify functionment on Windows and MacOs
+- Create a Tkinter interface
+- Create a system of leveling
+- Create a system of 1vAI
+- Make sure that `server.py` doen't crash if clients crash 
+- Send a message to one client if the other crash
+- Put network functions into a library and use this library or use another library/framework
+- Put game function into another file and use it
+- Put verbose mod for `client.py` and `server.py`
+- Make a function who find ip address automatically instead of localhost
+- Create a loop that enable to reask a response to client if the answer don't correspond to what we want
+- Upgrade network protocol
+- Make a multiplatform server (several combat in same time)
+- Bug on client when user write nothing when we ask him
+- Make a class for players
+- Put message to client when server stop but not clients
+- Document the network protocol used
 
+## Reporting and Contribution
+Don't hesitate to create issue and do some Pull Request if you want to participate/upgrade this project, ask questions or do some comments on this. Enjoy!
--- client.py
+++ client.py
@@ -2,7 +2,6 @@
 
 import socket
 
-#Adresse et port de mon Rpi3
 host = ""
 port = 3333
 
@@ -10,14 +9,16 @@ port = 3333
 
 def send_msg (msg):
     """
-    Envoie un message au serveur
+    Send a message to server
+    Argument: msg
     """
     data = msg.encode("utf-8")
     socket.sendall(data)
 
 def recv_msg ():
     """
-    Permet de recevoir message du serveur et le retourne en sortie
+    Receive a message from server
+    Return: msg
     """
     data = socket.recv(255)
     msg = data.decode("utf-8") 
@@ -28,46 +29,45 @@ def recv_msg ():
 socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 try :
-    socket.connect((host,port)) #On se connecte au serveur
-    print("Client connecté au Client") 
-    print("Demande d'un id_player au serveur...")
-    id_player = recv_msg() #On récupère un identifiant
+    socket.connect((host,port))             # Connecting to serveur
+    print("Client connected to server") 
+    print("Ask id_player to server...")
+    id_player = recv_msg()                  # Receive id
     if (id_player != ""):
-        send_msg("OK") #On dit au serveur que l'on a bien recu l'identifiant
-        print("Connecté avec l'identifiant", id_player)
+        send_msg("OK")                      # Send "OK" for id
+        print("Connected with id_player ", id_player)
     else:
-        print ("non connecté")
+        print ("Disconnected")
         exit(1)
-    if (recv_msg() == "READY"): #Si le serveur est pret
-            send_msg("READY") #On lui répond que l'on est prêt
+    if (recv_msg() == "READY"):             # If server is ready
+            send_msg("READY")               # We answer "READY"
     else:
-        print("Déconnexion du serveur")
+        print("Disconnected from server")
         socket.close()
         exit(2)
 
-### D'abord le serveur nous dit quoi faire, puis on agit selon ce qu'il dit
-    status = recv_msg()
-    while ((status != "" )and( status != "STOP")):
-        if (status == "READ"):
-            send_msg("OK")          #On signal que l'on est prêt à la reception
-            msg = recv_msg()        #On reçoit le message
-            print(msg)
-            if (msg != ""):         #Si on a bien reçu quelque chose
-                status = "OK"       #On demande au client de renvoyer ok
-        if (status == "WRITE"):
-            msg = input('-> ')       #L'utilisateur rentre un message
-            send_msg(msg)           #On envoie son message
-            status = recv_msg()     #On reçois le status
-        if (status == "OK"):
-            send_msg("OK")
-        #############TEST
-        if (status == "WRITE2"):
-            msg = input('-> ')       #L'utilisateur rentre un message
-            send_msg("OK")           #On envoie que son message a bien etait composé
-        if (status == "SEND"):
-            send_msg(msg)            #On envoie msg
+### Communication
+    status = recv_msg()             # We receive a status from server
+    while ((status != "" )and( status != "STOP")): 
+        if (status == "READ"):      # If he wants to print something
+            send_msg("OK")          # We answer "OK"
+            msg = recv_msg()        # He send the message
+            print(msg)              # We print it
+            if (msg != ""):         # And if it was ok
+                status = "OK"       # We answer "OK"
+        if (status == "WRITE"):     # If we wants some information
+            msg = input('-> ')      # User enter a message
+            send_msg(msg)           # And we send it
+            #status = recv_msg()    
+        if (status == "OK"):        # If we receive "OK"
+            send_msg("OK")          # We answer "OK"
+        if (status == "WRITE2"):    # Write v2 : allow to send message in same time that other user
+            msg = input('-> ')      # User enter a message
+            send_msg("OK")          # We say that we are ok for sending
+        if (status == "SEND"):      # If he wants the message,
+            send_msg(msg)           # We send it
 
         status = recv_msg()
 
 finally:       
-    socket.close()
+    socket.close()                  # Close the socket
