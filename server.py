13efb488380112e36ac428ebb3b4d84cd3922753
jerem <jeremy.lardenois@hotmail.fr>
2021-12-28 18:22:41 +0100

    use game lib for server

--- server.py
+++ server.py
@@ -1,70 +1,74 @@
 #!/usr/bin/env python3
-#coding:utf-8
+# coding:utf-8
 
+from lib.game import game
+from lib.network import send_msg, recv_msg
+import socket
 host = ""
 port = 3333
 
-import socket
-
-from lib.network import send_msg, recv_msg
-from lib.game import game
 
 ##################################MAIN#############################################
-socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)   # Type de connexion
-socket.bind((host,port))                                    # Establishment of the server connection
-print("Launching server") 
-socket.listen(3)                                            # Number of attempts allowed
+socket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)   # Connection type
+# Establishment of the server connection
+socket.bind((host, port))
+print("Launching server")
+# Number of attempts allowed
+socket.listen(3)
 
-while True:                                                 # The server doesn't close at the end of the game
-    id_player = 1
+try:
+    while True:                                                 # The server doesn't close at the end of the game
+        id_player = 1
 
-    # Clients connection
-    conn1, addr1 = socket.accept()                          # Waiting for clients
-    print(conn1)
-    print("Connexion", id_player, "accepté")
+        # Clients connection
+        conn1, addr1 = socket.accept()                          # Waiting for clients
+        print(conn1)
+        print("Connection", id_player, "accepted")
 
-    send_msg(conn1,id_player)
-    msg = recv_msg(conn1)
-    if (msg == "OK"):
-        print (id_player,"joueur(s) connecté(s)")
-        print(addr1)
-        id_player += 1
-    else:
-        print("echec de la connection")
-        exit(1) # Stop server
-    
-    # Second client
-    conn2, addr2 = socket.accept()
-    print("Connexion", id_player, "accepté")
+        send_msg(conn1, id_player)
+        msg = recv_msg(conn1)
+        if (msg == "OK"):
+            print(id_player, "connected players")
+            print(addr1)
+            id_player += 1
+        else:
+            print("Connection failed")
+            print("Exiting...")
+            exit(1)  # Stop server
 
-    send_msg(conn2,id_player)
-    msg = recv_msg(conn2)
-    if (msg == "OK"):
-        print (id_player,"joueur(s) connecté(s)")
-        print(addr2)
-        id_player += 1
-    else:
-        print("echec de la connection")
-        exit(1) # Stop server
+        # Second client
+        conn2, addr2 = socket.accept()
+        print("Connection", id_player, "accepted")
 
-    send_msg(conn1,"READY")
-    send_msg(conn2,"READY")
+        send_msg(conn2, id_player)
+        msg = recv_msg(conn2)
+        if (msg == "OK"):
+            print(id_player, "connected players")
+            print(addr2)
+            id_player += 1
+        else:
+            print("connection failed")
+            print("Exiting...")
+            exit(1)  # Stop server
 
-    msg1 = recv_msg(conn1)
-    msg2 = recv_msg(conn2)
-    if ((msg1 == "READY")and(msg2 == "READY")):
-        print("CONNECTION SUCCESSFULL")
+        send_msg(conn1, "READY")
+        send_msg(conn2, "READY")
 
-    ###################################
-    game(conn1,conn2) # Launch the game
-    ###################################
+        msg1 = recv_msg(conn1)
+        msg2 = recv_msg(conn2)
+        if ((msg1 == "READY") and (msg2 == "READY")):
+            print("CONNECTION SUCCESSFULL")
 
-    #Déconnexion
-    send_msg(conn1,"STOP") 
-    send_msg(conn2,"STOP")
-    conn1.close() 
-    conn2.close() 
+        ###################################
+        game(conn1, conn2)  # Launch the game
+        ###################################
 
-socket.close()
+        # Déconnexion
+        send_msg(conn1, "STOP")
+        send_msg(conn2, "STOP")
+        conn1.close()
+        conn2.close()
 
-
+finally:
+    socket.close()
