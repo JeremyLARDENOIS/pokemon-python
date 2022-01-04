28b82928936d6f48f2058a4005fecaa2b74fe360
JeremyLARDENOIS <jeremy.lardenois@hotmail.fr>
2022-01-04 20:28:14 +0100

    Remove players array

--- lib/game.py
+++ lib/game.py
@@ -8,7 +8,6 @@ def game(user1, user2):
     Arguments: conn1, conn2
     """
     conns = [user1.conn, user2.conn]
-    players = [user1, user2]
 
     broadcast(
         conns, "=========================================================================")
@@ -32,17 +31,17 @@ def game(user1, user2):
 
     broadcast(conns, "But first of all, what is your name?")
 
-    players[0].name, players[1].name = recv2(conns)
+    user1.name, user2.name = recv2(conns)
 
     nb_turns = 1
 
     # While the two players isn't K.O.
-    while ((players[0].hp > 0) and (players[1].hp > 0)):
+    while ((user1.hp > 0) and (user2.hp > 0)):
         broadcast(
             conns, "=========================================================================")
         broadcast(conns, "turn n°"+str(nb_turns)+" :")
-        broadcast(conns, "\n"+players[0].name+" = "+str(players[0].hp) +
-                  " hp & "+players[1].name+" = "+str(players[1].hp)+" hp")
+        broadcast(conns, "\n"+user1.name+" = "+str(user1.hp) +
+                  " hp & "+user2.name+" = "+str(user2.hp)+" hp")
 
         nb_turns += 1
 
@@ -56,59 +55,59 @@ def game(user1, user2):
         # Implementation of them
         # Heals are executed before attacks
         if (choix1 == "2"):                     # Heal of player 1
-            broadcast(conns, players[0].name+" heals himself!")
+            broadcast(conns, user1.name+" heals himself!")
             # We don't want to exceed the basic hp limit
-            gain = min(User().hp, players[0].hp +
-                       players[0].heal) - players[0].hp
-            players[0].hp += gain
-            broadcast(conns, players[0].name+" won "+str(gain)+" hp.\n")
+            gain = min(User().hp, user1.hp +
+                       user1.heal) - user1.hp
+            user1.hp += gain
+            broadcast(conns, user1.name+" won "+str(gain)+" hp.\n")
         if (choix2 == "2"):                     # Heal of player 2
-            broadcast(conns, players[1].name+" heals himself!")
+            broadcast(conns, user2.name+" heals himself!")
             # We don't want to exceed the basic hp limit
-            gain = min(User().hp, players[1].hp +
-                       players[1].heal) - players[1].hp
-            players[1].hp += gain
-            broadcast(conns, players[1].name+" won "+str(gain)+" hp.\n")
+            gain = min(User().hp, user2.hp +
+                       user2.heal) - user2.hp
+            user2.hp += gain
+            broadcast(conns, user2.name+" won "+str(gain)+" hp.\n")
         if (choix1 == "1"):                     # Attack of player 1
-            broadcast(conns, players[0].name + " attack!")
+            broadcast(conns, user1.name + " attack!")
             if (choix2 == "3"):                 # If player 2 do counter attack
-                broadcast(conns, "But " + players[1].name + " counter-attack!")
-                players[0].hp -= players[1].catk
-                broadcast(conns, players[0].name+" lost " +
-                          str(players[1].catk) + " hp.\n")
+                broadcast(conns, "But " + user2.name + " counter-attack!")
+                user1.hp -= user2.catk
+                broadcast(conns, user1.name+" lost " +
+                          str(user2.catk) + " hp.\n")
             else:                               # If player 2 doesn't counter-attack
-                players[1].hp -= players[0].atk
-                broadcast(conns, players[1].name +
-                          " lost "+str(players[0].atk)+" hp.\n")
+                user2.hp -= user1.atk
+                broadcast(conns, user2.name +
+                          " lost "+str(user1.atk)+" hp.\n")
         if (choix2 == "1"):                     # Attack of player 2
-            broadcast(conns, players[1].name+" attack!")
+            broadcast(conns, user2.name+" attack!")
             if (choix1 == "3"):                 # Counter-attack of player 1
-                broadcast(conns, "But "+players[0].name+" counter-attack!")
-                players[1].hp -= players[0].catk
-                broadcast(conns, players[1].name +
-                          " lost "+str(players[0].catk)+" hp.\n")
+                broadcast(conns, "But "+user1.name+" counter-attack!")
+                user2.hp -= user1.catk
+                broadcast(conns, user2.name +
+                          " lost "+str(user1.catk)+" hp.\n")
             else:
-                players[0].hp -= players[1].atk
-                broadcast(conns, players[0].name +
-                          " lost "+str(players[1].atk)+" hp.\n")
+                user1.hp -= user2.atk
+                broadcast(conns, user1.name +
+                          " lost "+str(user2.atk)+" hp.\n")
         if ((choix1 == "3") and (choix2 != "1")):
-            broadcast(conns, players[0].name + " counter-attack!")
+            broadcast(conns, user1.name + " counter-attack!")
             broadcast(conns, "But it's useless...\n")
-            players[0].hp -= players[0].costca
-            broadcast(conns, players[0].name+" lost " +
-                      str(players[0].costca)+" hp.\n")
+            user1.hp -= user1.costca
+            broadcast(conns, user1.name+" lost " +
+                      str(user1.costca)+" hp.\n")
         # If player 2 do counter attack and player 1 doesn't attack
         if ((choix2 == "3") and (choix1 != "1")):
-            broadcast(conns, players[1].name+" counter-attack!")
+            broadcast(conns, user2.name+" counter-attack!")
             broadcast(conns, "But it's useless...\n")
-            players[1].hp -= players[1].costca
-            broadcast(conns, players[1].name+" lost " +
-                      str(players[1].costca)+" hp.\n")
+            user2.hp -= user2.costca
+            broadcast(conns, user2.name+" lost " +
+                      str(user2.costca)+" hp.\n")
 
-    if ((players[0].hp <= 0) and (players[1].hp <= 0)):
+    if ((user1.hp <= 0) and (user2.hp <= 0)):
         broadcast(conns, "Both players have no more hp, it's a tie!")
     else:
-        if (players[1].hp <= 0):
-            broadcast(conns, players[0].name+" won!")
-        if (players[0].hp <= 0):
-            broadcast(conns, players[1].name+" won!")
+        if (user2.hp <= 0):
+            broadcast(conns, user1.name+" won!")
+        if (user1.hp <= 0):
+            broadcast(conns, user2.name+" won!")
